0c77fd0d91072f8ecfec9e9e4e47b70e01798ddf
Jeanre <jeanre.peacock@gmail.com>
2020-11-12 16:29:11 +0200

    Initial Commit

---
+++ MPS6_Banking_Mobile/MPS6_Banking_Mobile/Deposit.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MPS6_Banking_Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MPS6_Banking_Mobile/MPS6_Banking_Mobile/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Banking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MPS6_Banking_Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        private BankAccount _account;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Bank fnb = new Bank("First National Bank", 4324, "Kenilworth");
+            Customer myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
+            fnb.AddCustomer(myNewCustomer);
+
+            _account = myNewCustomer.ApplyForBankAccount();
+        }
+
+        private void DepositButton_Clicked(object sender, EventArgs e)
+        {
+            decimal depositAmount = 0;
+            var valid = decimal.TryParse(DepositAmountEntry.Text, out depositAmount);
+            var reason = DepositReasonEntry.Text;
+
+            if (valid)
+            {
+                _account.DepositMoney(depositAmount, DateTime.Now, reason);
+            }
+            else
+            {
+                DisplayAlert("Validation Error", "Please Enter a Number", "Cancel");
+            }
+        }
+    }
+}
